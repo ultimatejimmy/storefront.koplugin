3232e66a36a8d2fd992f07b83ed57cc5cbde7db2
ömer faruk inel <omer.inel@ciellos.com>
2026-04-21 21:21:37 +0300

    v1.2.0

--- _meta.lua
+++ _meta.lua
@@ -4,6 +4,6 @@ return {
     name = "appstore",
     fullname = _("App Store"),
     description = _([[Discover and manage community plugins and patches from GitHub topics.]]),
-    version = "1.1.5",
+    version = "1.2.0",
 }
 
