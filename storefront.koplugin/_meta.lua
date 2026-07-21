e61af514485d1adec90aaa5b40c4fafe076e4969
Jimmy Pautz <jpautz@watermarkinsights.com>
2026-07-21 14:17:46 -0500

    Release 26.7.21.3-beta

--- storefront.koplugin/_meta.lua
+++ storefront.koplugin/_meta.lua
@@ -4,7 +4,7 @@ return {
     name = "storefront",
     fullname = _("Storefront"),
     description = _([[Discover and manage community plugins and patches from GitHub topics.]]),
-    version = "26.7.21.2-beta",
+    version = "26.7.21.3-beta",
     author = "ultimatejimmy",
 }
 
