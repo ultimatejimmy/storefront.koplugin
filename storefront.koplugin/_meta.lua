a1a488991a7c24d619835ffda0642373f634d1fe
Jimmy Pautz <jimmy@jimmypautz.com>
2026-08-16 14:40:35 -0500

    Release 26.8.16-beta

--- storefront.koplugin/_meta.lua
+++ storefront.koplugin/_meta.lua
@@ -12,6 +12,6 @@ end
 return {
     fullname = _("menu_storefront"),
     description = _("menu_storefront_desc"),
-    version = "26.8.15-alpha4",
+    version = "26.8.16-beta",
     author = "ultimatejimmy",
 }
