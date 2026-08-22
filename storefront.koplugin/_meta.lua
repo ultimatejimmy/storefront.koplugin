2ca88c4b9ea3ee5fd89af885f4e069ade696bcf8
Jimmy Pautz <jpautz@watermarkinsights.com>
2026-08-21 23:44:49 -0500

    Release 26.8.21-beta

--- storefront.koplugin/_meta.lua
+++ storefront.koplugin/_meta.lua
@@ -12,6 +12,6 @@ end
 return {
     fullname = _("menu_storefront"),
     description = _("menu_storefront_desc"),
-    version = "26.8.20-beta",
+    version = "26.8.21-beta",
     author = "ultimatejimmy",
 }
